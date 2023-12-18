f9db8a1046d3161fd8cf0551656158639bf0ad1e
Marcelo Baranowski <marcelobaranowski@hotmail.com>
2023-12-18 20:49:57 -0300

    Routes y Controllers

---
+++ src/controllers/mainController.js
@@ -0,0 +1,8 @@
+const mainControllers = {
+    home: (req, res) => res.send('Esta es la vista de HOME'),
+    contact: (req, res) => res.send('Esta es la vista de CONTACTO'),
+    about: (req, res) => res.send('Esta es la vista SOBRE NOSOTROS'),
+    faqs: (req, res) => res.send('Esta es la vista de PREGUNTAS FRECUENTES'),
+}
+
+module.exports = mainControllers
--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router;
 
-router.get('/home', (rep, req) => res.send('Esta es la vista de HOME'));
-router.get('/contact', (rep, req) => res.send('Esta es la vista de CONTACTO'));
-router.get('/about', (rep, req) => res.send('Esta es la vista SOBRE NOSOTROS'));
-router.get('/faqs', (rep, req) => res.send('Esta es la vista de PREGUNTAS FRECUENTES'));
+const controller = require('../controllers/mainController');
+
+router.get('/home', controller.home);
+router.get('/contact', controller.contact);
+router.get('/about', controller.about);
+router.get('/faqs', controller.faqs);
 
 module.exports = router;
