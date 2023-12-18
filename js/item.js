8b4e0bab8727d0f1be372777f35cb34fa7c4646d
Marcelo Baranowski <marcelobaranowski@hotmail.com>
2023-12-18 15:38:20 -0300

    ajuste estilos

--- js/item.js
+++ js/item.js
@@ -4,8 +4,9 @@ const quantity = document.querySelector('#quantity');
 
 add.addEventListener('click', () => quantity.value = Number(quantity.value) + 1);
 subtract.addEventListener('click', () => {
-    
-    quantity.value = Number(quantity.value) + -1
-    
+  quantity.value = Number(quantity.value) === 0
+    ? 0
+    : Number(quantity.value) - 1
 });
+quantity.addEventListener('change', () => quantity.value = Number(quantity.value) < 0 ? 0 : Number(quantity.value));
 
